e90207398d06de8b175a15047ea9e7417a5bdb54
Michal Manak <fredybotas@gmail.com>
2021-07-06 22:03:49 +0200

    Move requesting for healthkit permissions to workout service

--- HeartZones WatchKit Extension/ExtensionDelegate.swift
+++ HeartZones WatchKit Extension/ExtensionDelegate.swift
@@ -36,30 +36,10 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
         
         return container
     }()
-    
-    func authorizeHealthKitAccess(toRead readable: Set<HKObjectType>?, toWrite writable: Set<HKSampleType>?, completion: @escaping (Bool, HKError.Code?) -> Void)
-    {
-        guard HKHealthStore.isHealthDataAvailable() else {
-            completion(false, .errorHealthDataUnavailable)
-            return
-        }
-        HKHealthStore().requestAuthorization(toShare: writable, read: readable) { (authorized, error) in
-            guard authorized else {
-                guard error != nil else {
-                    completion(false, .noError)
-                    return
-                }
-                print("HealthKit Error:\n\(error!)") // Comment this out for release
-                completion(false, .errorAuthorizationDenied)
-                return
-            }
-            completion(true, nil)
-        }
-    }
 
     func applicationDidFinishLaunching() {
         // Perform any final initialization of your application.
-        authorizeHealthKitAccess(toRead: [
+        WorkoutService.authorizeHealthKitAccess(toRead: [
             HKQuantityType.quantityType(forIdentifier: .heartRate)!,
             HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!,
             HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
