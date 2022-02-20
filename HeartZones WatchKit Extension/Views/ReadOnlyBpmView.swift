2b8fe4868c1830dd3f024b6320e4889a66ec7346
Michal Manak <fredybotas@gmail.com>
2022-02-20 12:46:50 +0100

    Move ReadOnlyBpmView subviews to namespace

--- HeartZones WatchKit Extension/Views/ReadOnlyBpmView.swift
+++ HeartZones WatchKit Extension/Views/ReadOnlyBpmView.swift
@@ -7,63 +7,63 @@
 
 import SwiftUI
 
+struct ReadOnlyBpmView: View {
+    struct IndicatorView: View {
+        let percentage: UInt
+        private let kHeight: CGFloat = 20
+        private let kRadius: CGFloat = 8
+        private let kSpacing: CGFloat = 1
 
-struct IndicatorView: View {
-    let percentage: UInt
-    private let kHeight: CGFloat = 20
-    private let kRadius: CGFloat = 8
-    private let kSpacing: CGFloat = 1
-    
-    func getRectangleBaseWidth(_ geo: GeometryProxy) -> CGFloat {
-        // TODO: Change 4 to actual rectangle count
-        let rectangleCount = 4
-        return geo.size.width - CGFloat(rectangleCount - 1) * kSpacing
-    }
-    var body: some View {
-        GeometryReader { geo in
-            ZStack(alignment: .trailing) {
-                HStack(spacing: kSpacing) {
-                    RoundedRectangle(cornerRadius: kRadius)
-                        .frame(width: getRectangleBaseWidth(geo) * 0.3, height: kHeight)
-                        .foregroundColor(Color.blue)
-                    RoundedRectangle(cornerRadius: kRadius)
-                        .frame(width: getRectangleBaseWidth(geo) * 0.2, height: kHeight)
-                        .foregroundColor(Color.green)
-                    RoundedRectangle(cornerRadius: kRadius)
-                        .frame(width: getRectangleBaseWidth(geo) * 0.2, height: kHeight)
-                        .foregroundColor(Color.orange)
-                    RoundedRectangle(cornerRadius: kRadius)
-                        .frame(width: getRectangleBaseWidth(geo) * 0.3, height: kHeight)
-                        .foregroundColor(Color.red)
+        func getRectangleBaseWidth(_ geo: GeometryProxy) -> CGFloat {
+            // TODO: Change 4 to actual rectangle count
+            let rectangleCount = 4
+            return geo.size.width - CGFloat(rectangleCount - 1) * kSpacing
+        }
+
+        var body: some View {
+            GeometryReader { geo in
+                ZStack(alignment: .trailing) {
+                    HStack(spacing: kSpacing) {
+                        RoundedRectangle(cornerRadius: kRadius)
+                            .frame(width: getRectangleBaseWidth(geo) * 0.3, height: kHeight)
+                            .foregroundColor(Color.blue)
+                        RoundedRectangle(cornerRadius: kRadius)
+                            .frame(width: getRectangleBaseWidth(geo) * 0.2, height: kHeight)
+                            .foregroundColor(Color.green)
+                        RoundedRectangle(cornerRadius: kRadius)
+                            .frame(width: getRectangleBaseWidth(geo) * 0.2, height: kHeight)
+                            .foregroundColor(Color.orange)
+                        RoundedRectangle(cornerRadius: kRadius)
+                            .frame(width: getRectangleBaseWidth(geo) * 0.3, height: kHeight)
+                            .foregroundColor(Color.red)
+                    }
+                    Rectangle()
+                        .frame(width: geo.size.width * (1.0 - (CGFloat(percentage) / 100.0)), height: kHeight)
+                        .foregroundColor(Color.black)
+                        .opacity(0.75)
                 }
-                Rectangle()
-                    .frame(width: (geo.size.width * (1.0 - (CGFloat(percentage) / 100.0))), height: kHeight)
-                    .foregroundColor(Color.black)
-                    .opacity(0.75)
+            }
+            .frame(height: kHeight)
+        }
+    }
+
+    struct BpmView: View {
+        let bpm: UInt
+
+        var body: some View {
+            HStack(alignment: .bottom, spacing: 2) {
+                Text("144")
+                    .minimumScaleFactor(0.6)
+                    .lineLimit(1)
+                    .foregroundColor(Color.orange)
+                    .font(
+                        Font.system(size: 62, weight: .light, design: .default)
+                    )
+                    .frame(width: 100, alignment: .leading)
             }
         }
-        .frame(height: kHeight)
     }
-}
 
-struct BpmView: View {
-    let bpm: UInt
-
-    var body: some View {
-        HStack(alignment: .bottom, spacing: 2) {
-            Text("144")
-                .minimumScaleFactor(0.6)
-                .lineLimit(1)
-                .foregroundColor(Color.orange)
-                .font(
-                    Font.system(size: 62, weight: .light, design: .default)
-                )
-                .frame(width: 100, alignment: .leading)
-        }
-    }
-}
-
-struct ReadOnlyBpmView: View {
     var body: some View {
         VStack {
             IndicatorView(percentage: 60)
